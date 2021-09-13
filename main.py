46c129d6251a5501d6cd19efebf78fdc8f359674
Sergei Pakulin <pakylin3@mail.ru>
2021-09-13 12:49:44 +0500

    +local envs for user

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from werkzeug.contrib.fixers import ProxyFix
 from app import create_app, db, socketio
 
 if __name__ == "__main__":
+    load_dotenv("local.env")
     load_dotenv("oauth.env")
     load_dotenv("mongo_conn_string.env")
 
