090eb9f836c83426aef07d214426557ae26c8b14
Sergei Pakulin <pakylin3@mail.ru>
2021-09-13 13:20:40 +0500

    typed analytics/service.py

--- app/api/analytics/service.py
+++ app/api/analytics/service.py
@@ -1,16 +1,29 @@
+import typing as npt
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+import numpy
+import numpy as np
 from app.api.composer.service import composer_history_for_case
 from app.api.data.service import get_input_data
-from .models import PlotData, BoxPlotData
-import numpy as np
+from app.api.showcase.models import ShowcaseItem
+from fedot.core.data.data import InputData, OutputData
+from fedot.core.optimisers.opt_history import OptHistory
+from fedot.core.pipelines.pipeline import Pipeline
 
-max_items_in_plot = 50
+from .models import BoxPlotData, PlotData
+
+max_items_in_plot: int = 50
 
 
-def _make_chart_dicts_for_boxplot(x, ys, x_title, y_title):
-    series = []
+def _make_chart_dicts_for_boxplot(
+    x: List[int],
+    ys: List[List[int]],
+    x_title: str, y_title: str
+) -> List[Dict[str, Union[str, int, list]]]:
+    series: List[Dict[str, Union[str, int, list]]] = []
 
     for i in range(len(ys)):
-        y = [round(_, 3) for _ in ys[i]]
+        y: List[int] = [round(_, 3) for _ in ys[i]]
         series.append({
             'y': f'Gen {x[i]}',
             'x': y,
@@ -28,8 +41,16 @@ def _process_y_value(y):
     return round(y_new, 3)
 
 
-def _make_chart_dicts(x, ys, names, x_title, y_title, plot_type, y_bnd=None):
-    series = []
+def _make_chart_dicts(
+    x: List[int],
+    ys: List[List[int]],
+    names: List[str],
+    x_title: str, y_title: str, plot_type: str, y_bnd=None
+) -> Tuple[
+    List[Dict[str, Union[str, List[int], List[List[int]]]]],
+    Dict[str, Dict[str, Any]]
+]:
+    series: List[Dict[str, Union[str, List[int], List[List[int]]]]] = []
 
     for i in range(len(ys)):
         if plot_type == 'line':
@@ -48,7 +69,7 @@ def _make_chart_dicts(x, ys, names, x_title, y_title, plot_type, y_bnd=None):
     else:
         min_y, max_y = y_bnd
 
-    options = {
+    options: Dict[str, Dict[str, Any]] = {
         'chart': {
             'type': plot_type
         },
@@ -69,11 +90,11 @@ def _make_chart_dicts(x, ys, names, x_title, y_title, plot_type, y_bnd=None):
     return series, options
 
 
-def get_quality_analytics(case_id) -> PlotData:
-    history = composer_history_for_case(case_id)
+def get_quality_analytics(case_id: str) -> PlotData:
+    history: OptHistory = composer_history_for_case(case_id)
 
-    y = [round(abs(min([i.fitness for i in gen])), 3) for gen in history.individuals]
-    x = list(range(len(history.individuals)))
+    y: List[int] = [round(abs(min([i.fitness for i in gen])), 3) for gen in history.individuals]
+    x: List[int] = list(range(len(history.individuals)))
 
     series, options = _make_chart_dicts(x=x, ys=[y], names=['Test sample'],
                                         x_title='Epochs', y_title='Fitness',
@@ -83,58 +104,58 @@ def get_quality_analytics(case_id) -> PlotData:
     return output
 
 
-def get_population_analytics(case_id, analytic_type: str) -> BoxPlotData:
-    history = composer_history_for_case(case_id)
+def get_population_analytics(case_id: str, analytic_type: str) -> BoxPlotData:
+    history: OptHistory = composer_history_for_case(case_id)
 
     if analytic_type == 'pheno':
-        y_gen = [[abs(i.fitness) for i in gen] for gen in history.individuals]
+        y_gen: List[List[int]] = [[abs(i.fitness) for i in gen] for gen in history.individuals]
     elif analytic_type == 'geno':
-        y_gen = [[abs(i.graph.depth) for i in gen] for gen in history.individuals]
+        y_gen: List[List[int]] = [[abs(i.graph.depth) for i in gen] for gen in history.individuals]
     else:
         raise ValueError(f'Analytic type {analytic_type} not recognized')
 
-    x = list(range(len(history.individuals)))
+    x: List[int] = list(range(len(history.individuals)))
 
-    series = _make_chart_dicts_for_boxplot(x=x, ys=y_gen,
+    series: List[Dict[str, Union[str, int, list]]] = _make_chart_dicts_for_boxplot(x=x, ys=y_gen,
                                            x_title='Epochs', y_title='Fitness')
 
     output = BoxPlotData(series=series)
     return output
 
 
-def _test_prediction_for_pipeline(case, pipeline):
-    train_data = get_input_data(dataset_name=case.metadata.dataset_name, sample_type='train')
+def _test_prediction_for_pipeline(
+    case: ShowcaseItem, pipeline: Optional[Pipeline]
+) -> Tuple[Optional[InputData], OutputData]:
+    train_data: Optional[InputData] = get_input_data(dataset_name=case.metadata.dataset_name, sample_type='train')
 
     if not pipeline.is_fitted:
         pipeline.fit(train_data)
 
-    test_data = get_input_data(dataset_name=case.metadata.dataset_name, sample_type='test')
-    prediction = pipeline.predict(test_data)
+    test_data: Optional[InputData] = get_input_data(dataset_name=case.metadata.dataset_name, sample_type='test')
+    prediction: OutputData = pipeline.predict(test_data)
     return test_data, prediction
 
 
-def get_modelling_results(case, pipeline, baseline_pipeline=None) -> PlotData:
-    test_data, prediction = _test_prediction_for_pipeline(case, pipeline)
-
-    if case.metadata.task_name == 'ts_forecasting':
-        obs = test_data.target
-    else:
-        obs = list([float(o[0]) for o in test_data.target])
-
-    baseline_prediction = None
+def get_modelling_results(
+    case: ShowcaseItem,
+    pipeline: Optional[Pipeline],
+    baseline_pipeline: Optional[Pipeline]=None
+) -> PlotData:
+    _, prediction = _test_prediction_for_pipeline(case, pipeline)
+    baseline_prediction: Optional[OutputData] = None
     if baseline_pipeline:
         _, baseline_prediction = _test_prediction_for_pipeline(case, baseline_pipeline)
-    y_bnd = None
+    y_bnd: Optional[Tuple[int, ...]] = None
     if case.metadata.task_name == 'classification':
-        y_title = 'Probability'
-        x_title = 'Item'
+        y_title: str = 'Probability'
+        x_title: str = 'Item'
         y_bnd = (0, 1)
     elif case.metadata.task_name == 'regression':
-        y_title = 'Value'
-        x_title = 'Item'
+        y_title: str = 'Value'
+        x_title: str = 'Item'
     elif case.metadata.task_name == 'ts_forecasting':
-        y_title = 'Value'
-        x_title = 'Time step'
+        y_title: str = 'Value'
+        x_title: str = 'Time step'
     else:
         raise NotImplementedError(f'Task {case.metadata.task_name} not supported')
 
@@ -151,11 +172,11 @@ def get_modelling_results(case, pipeline, baseline_pipeline=None) -> PlotData:
             y_baseline = list(baseline_prediction.predict) if baseline_prediction else None
         y_obs = obs
 
-    x = list(range(len(prediction.predict)))
+    x: List[int] = list(range(len(prediction.predict)))
     x = x[:max_items_in_plot]
-    y = y[:max_items_in_plot]
-    ys = [y]
-    names = ['Candidate']
+    y: List[int] = y[:max_items_in_plot]
+    ys: List[List[int]] = [y]
+    names: List[str] = ['Candidate']
     if baseline_prediction:
         y_baseline = y_baseline[:max_items_in_plot]
         ys.append(y_baseline)
