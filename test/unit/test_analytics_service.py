4881600aff26650534d1608aae180deb985432b7
Sergei Pakulin <pakylin3@mail.ru>
2021-09-22 14:24:27 +0500

    fixed chart funcs testing in unit/test_analytics_service.py
    - computing truthy result in test cases, instead prepared correct
    + approx dict comparison

--- test/unit/test_analytics_service.py
+++ test/unit/test_analytics_service.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
-from typing import Any, Callable, List, Optional, Tuple, Union
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 import numpy as np
+import pandas as pd
 import pytest
 from app.api.analytics.service import (_make_chart_dicts,
                                        _make_chart_dicts_for_boxplot,
@@ -9,6 +10,7 @@ from app.api.analytics.service import (_make_chart_dicts,
                                        get_population_analytics,
                                        get_prediction_for_pipeline,
                                        get_quality_analytics)
+from pandas.testing import assert_series_equal
 
 from .fixtures.analytics_service_fixtures import (
     box_plot_data_fixture, chart_dicts_fixture,
@@ -28,10 +30,126 @@ def _assert_arrays(given: List[Any], correct: List[Any], label: str = 'x'):
             assert giveni == pytest.approx(correcti), (
                 f'Incorrect {label}{idx} mapping: {label}{idx}={giveni} != correct_{label}{idx}={correcti}'
             )
+    elif given and type(given[0]) is dict:
+        assert_series_equal(pd.Series(given), pd.Series(correct))
     else:
         assert given == pytest.approx(correct), (
             f'Incorrect {label} mapping: {label}={given} != correct_{label}={correct}'
         )
+
+
+@dataclass
+class BoxplotChartTestCase:
+    x: List[int]
+    ys: List[List[Union[int, float]]]
+    x_title: str
+    y_title: str
+    correct_output: List[Dict]
+
+
+BOXPLOT_CHART_TEST_CASES = [
+    BoxplotChartTestCase(
+        x=[1, 2, 3],
+        ys=[
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ],
+        x_title='x',
+        y_title='y',
+        correct_output=[
+            {
+                'x': [1, 2, 3],
+                'y': 'Gen 1',
+                'type': 'box',
+                'name': 0
+            },
+            {
+                'x': [4, 5, 6],
+                'y': 'Gen 2',
+                'type': 'box',
+                'name': 1
+            },
+            {
+                'x': [7, 8, 9],
+                'y': 'Gen 3',
+                'type': 'box',
+                'name': 2
+            }
+        ],
+    ),
+    BoxplotChartTestCase(
+        x=[4, 5, 6],
+        ys=[
+            [1.2, 2.3, 3.4],
+            [4.5, 5.6, 6.7],
+            [7.8, 8.9, 9.10]
+        ],
+        x_title='x',
+        y_title='y',
+        correct_output=[
+            {
+                'x': [1.2, 2.3, 3.4],
+                'y': 'Gen 4',
+                'type': 'box',
+                'name': 0
+            },
+            {
+                'x': [4.5, 5.6, 6.7],
+                'y': 'Gen 5',
+                'type': 'box',
+                'name': 1
+            },
+            {
+                'x': [7.8, 8.9, 9.10],
+                'y': 'Gen 6',
+                'type': 'box',
+                'name': 2
+            }
+        ]
+    ),
+    BoxplotChartTestCase(
+        x=[10, 50, 100],
+        ys=[
+            [0.00199, 0.01999, 0.19999],
+            [0.5555, 0, 0],
+            [7, 8, 9]
+        ],
+        x_title='x',
+        y_title='y',
+        correct_output=[
+            {
+                'x': [0.002, 0.02, 0.2],
+                'y': 'Gen 10',
+                'type': 'box',
+                'name': 0
+            },
+            {
+                'x': [0.555, 0, 0],
+                'y': 'Gen 50',
+                'type': 'box',
+                'name': 1
+            },
+            {
+                'x': [7, 8, 9],
+                'y': 'Gen 100',
+                'type': 'box',
+                'name': 2
+            }
+        ],
+    )
+]
+
+
+@pytest.mark.parametrize('case', BOXPLOT_CHART_TEST_CASES)
+def test_make_chart_dicts_for_boxplot(case: BoxplotChartTestCase):
+    result = _make_chart_dicts_for_boxplot(
+        x=case.x,
+        ys=case.ys,
+        x_title=case.x_title,
+        y_title=case.y_title
+    )
+    _assert_arrays(result, case.correct_output, 'chart')
 
 
 @dataclass
@@ -41,7 +159,10 @@ class ChartTestCase:
     x_title: str
     y_title: str
     names: List[str]
+    plot_type: str
     y_bnd: Optional[Tuple[int, int]]
+    correct_series: List[Dict]
+    correct_options: Dict
 
 
 CHART_TEST_CASES = [
@@ -52,22 +173,261 @@ CHART_TEST_CASES = [
             [4, 5, 6],
             [7, 8, 9]
         ],
-        x_title='x',
-        y_title='y',
-        names=['test', 'sample', 'cases'],
-        y_bnd=None
+        x_title='x1',
+        y_title='y1',
+        names=['test1', 'sample1', 'cases1'],
+        plot_type='other',
+        y_bnd=None,
+        correct_series=[
+            {
+                'name': 'test1',
+                'data': [
+                    [1, 1],
+                    [2, 2],
+                    [3, 3]
+                ]
+            },
+            {
+                'name': 'sample1',
+                'data': [
+                    [1, 4],
+                    [2, 5],
+                    [3, 6]
+                ]
+            },
+            {
+                'name': 'cases1',
+                'data': [
+                    [1, 7],
+                    [2, 8],
+                    [3, 9]
+                ]
+            }
+        ],
+        correct_options={
+            'chart': {
+                'type': 'other'
+            },
+            'xaxis': {
+                'categories': [1, 2, 3],
+                'title': {
+                    'text': 'x1'
+                }
+            },
+            'yaxis': {
+                'title': {
+                    'text': 'y1'
+                },
+                'min': 0.95,
+                'max': 9.45
+            }
+        }
     ),
     ChartTestCase(
-        x=[4, 5, 6],
+        x=[1, 2, 3, 4],
+        ys=[
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9, 10, 11, 12]
+        ],
+        x_title='x2',
+        y_title='y2',
+        names=['test2', 'sample2', 'cases2'],
+        plot_type='line',
+        y_bnd=None,
+        correct_series=[
+            {
+                'name': 'test2',
+                'data': [1, 2, 3, 4]
+            },
+            {
+                'name': 'sample2',
+                'data': [5, 6, 7, 8]
+            },
+            {
+                'name': 'cases2',
+                'data': [9, 10, 11, 12]
+            }
+        ],
+        correct_options={
+            'chart': {
+                'type': 'line'
+            },
+            'xaxis': {
+                'categories': [1, 2, 3, 4],
+                'title': {
+                    'text': 'x2'
+                }
+            },
+            'yaxis': {
+                'title': {
+                    'text': 'y2'
+                },
+                'min': 0.95,
+                'max': 12.6
+            }
+        }
+    ),
+
+    ChartTestCase(
+        x=[0, 1, 2],
         ys=[
             [1.2, 2.3, 3.4],
             [4.5, 5.6, 6.7],
-            [7.8, 8.9, 9.10]
+            [7.8, 8.9, 9.1]
         ],
-        x_title='x',
-        y_title='y',
-        names=['test', 'sample', 'cases'],
-        y_bnd=(1.14, 9.555)
+        x_title='x3',
+        y_title='y3',
+        names=['test3', 'sample3', 'cases3'],
+        plot_type='other',
+        y_bnd=(1.14, 9.555),
+        correct_series=[
+            {
+                'name': 'test3',
+                'data': [
+                    [0, 1.2],
+                    [1, 2.3],
+                    [2, 3.4]
+                ]
+            },
+            {
+                'name': 'sample3',
+                'data': [
+                    [0, 4.5],
+                    [1, 5.6],
+                    [2, 6.7]
+                ]
+            },
+            {
+                'name': 'cases3',
+                'data': [
+                    [0, 7.8],
+                    [1, 8.9],
+                    [2, 9.1]
+                ]
+            }
+        ],
+        correct_options={
+            'chart': {
+                'type': 'other'
+            },
+            'xaxis': {
+                'categories': [0, 1, 2],
+                'title': {
+                    'text': 'x3'
+                }
+            },
+            'yaxis': {
+                'title': {
+                    'text': 'y3'
+                },
+                'min': 1.14,
+                'max': 9.555
+            }
+        }
+    ),
+    ChartTestCase(
+        x=[0, 1, 2],
+        ys=[
+            [1.2, 2.3, 3.4],
+            [4.5, 5.6, 6.7],
+            [7.8, 8.9, 9.1]
+        ],
+        x_title='x4',
+        y_title='y4',
+        names=['test4', 'sample4', 'cases4'],
+        plot_type='line',
+        y_bnd=(1.14, 9.555),
+        correct_series=[
+            {
+                'name': 'test4',
+                'data': [1.2, 2.3, 3.4]
+            },
+            {
+                'name': 'sample4',
+                'data': [4.5, 5.6, 6.7]
+            },
+            {
+                'name': 'cases4',
+                'data': [7.8, 8.9, 9.1]
+            }
+        ],
+        correct_options={
+            'chart': {
+                'type': 'line'
+            },
+            'xaxis': {
+                'categories': [0, 1, 2],
+                'title': {
+                    'text': 'x4'
+                }
+            },
+            'yaxis': {
+                'title': {
+                    'text': 'y4'
+                },
+                'min': 1.14,
+                'max': 9.555
+            }
+        }
+    ),
+
+    ChartTestCase(
+        x=[10, 50, 100],
+        ys=[
+            [0.00199, 0.01999, 0.19999],
+            [0.5555, 0, 0],
+            [7, 8, 9]
+        ],
+        x_title='x5',
+        y_title='y5',
+        names=['test5', 'sample5', 'cases5'],
+        plot_type='other',
+        y_bnd=(0, 9.45),
+        correct_series=[
+            {
+                'name': 'test5',
+                'data': [
+                    [10, 0.002],
+                    [50, 0.02],
+                    [100, 0.2]
+                ]
+            },
+            {
+                'name': 'sample5',
+                'data': [
+                    [10, 0.555],
+                    [50, 0],
+                    [100, 0]
+                ]
+            },
+            {
+                'name': 'cases5',
+                'data': [
+                    [10, 7],
+                    [50, 8],
+                    [100, 9]
+                ]
+            }
+        ],
+        correct_options={
+            'chart': {
+                'type': 'other'
+            },
+            'xaxis': {
+                'categories': [10, 50, 100],
+                'title': {
+                    'text': 'x5'
+                }
+            },
+            'yaxis': {
+                'title': {
+                    'text': 'y5'
+                },
+                'min': 0,
+                'max': 9.45
+            }
+        }
     ),
     ChartTestCase(
         x=[10, 50, 100],
@@ -76,82 +436,60 @@ CHART_TEST_CASES = [
             [0.5555, 0, 0],
             [7, 8, 9]
         ],
-        x_title='x',
-        y_title='y',
-        names=['test', 'sample', 'cases'],
-        y_bnd=(0, 9.45)
+        x_title='x6',
+        y_title='y6',
+        names=['test6', 'sample6', 'cases6'],
+        plot_type='line',
+        y_bnd=None,
+        correct_series=[
+            {
+                'name': 'test6',
+                'data': [0.002, 0.02, 0.2]
+            },
+            {
+                'name': 'sample6',
+                'data': [0.555, 0, 0]
+            },
+            {
+                'name': 'cases6',
+                'data': [7, 8, 9]
+            }
+        ],
+        correct_options={
+            'chart': {
+                'type': 'line'
+            },
+            'xaxis': {
+                'categories': [10, 50, 100],
+                'title': {
+                    'text': 'x6'
+                }
+            },
+            'yaxis': {
+                'title': {
+                    'text': 'y6'
+                },
+                'min': 0,
+                'max': 9.45
+            }
+        }
     )
 ]
 
 
 @pytest.mark.parametrize('case', CHART_TEST_CASES)
-def test_make_chart_dicts_for_boxplot(case: ChartTestCase):
-    result = _make_chart_dicts_for_boxplot(
+def test_make_chart_dicts(case: ChartTestCase):
+    result_series, result_options = _make_chart_dicts(
         x=case.x,
         ys=case.ys,
+        names=case.names,
         x_title=case.x_title,
-        y_title=case.y_title
+        y_title=case.y_title,
+        plot_type=case.plot_type,
+        y_bnd=case.y_bnd
     )
-    correct = [
-        {
-            'y': f'Gen {case.x[idx]}',
-            'x': [round(_, 3) for _ in y],
-            'type': 'box',
-            'name': idx
-        }
-        for idx, y in enumerate(case.ys)
-    ]
-    _assert_arrays(result, correct, 'chart')
-
-
-@pytest.mark.parametrize('case', CHART_TEST_CASES)
-def test_make_chart_dicts(case: ChartTestCase):
-    plot_types = ['line', 'other']
-    for plot_type in plot_types:
-        result_series, result_options = _make_chart_dicts(
-            x=case.x,
-            ys=case.ys,
-            names=case.names,
-            x_title=case.x_title,
-            y_title=case.y_title,
-            plot_type=plot_type,
-            y_bnd=case.y_bnd
-        )
-        correct_series = [
-            {
-                'name': case.names[idx1],
-                'data':
-                    [round(_, 3) for _ in y]
-                    if plot_type == 'line'
-                    else [[case.x[idx2], round(_, 3)] for idx2, _ in enumerate(y)]
-            }
-            for idx1, y in enumerate(case.ys)
-        ]
-        if not case.y_bnd:
-            min_y: float = min(min(y) for y in case.ys) * 0.95
-            max_y: float = max(max(y) for y in case.ys) * 1.05
-        else:
-            min_y, max_y = case.y_bnd
-        correct_options = {
-            'chart': {
-                'type': plot_type
-            },
-            'xaxis': {
-                'categories': case.x,
-                'title': {
-                    'text': case.x_title
-                }
-            },
-            'yaxis': {
-                'title': {
-                    'text': case.y_title
-                },
-                'min': min_y,
-                'max': max_y
-            }
-        }
-    _assert_arrays(result_series, correct_series, 'series')
-    _assert_arrays([result_options], [correct_options], 'options')
+    _assert_arrays(result_series, case.correct_series, 'series')
+    _assert_arrays([result_options], [case.correct_options], 'options')
 
 
 def test_get_quality_analytics(
